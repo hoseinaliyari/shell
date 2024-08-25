cd663d3f372d388b0478962c30716e5a743b0a5c
hoseinaliyari <ho3einaliyari@gmail.com>
2024-08-25 13:22:35 -0400

    Update assignment.sh
    realized that using 'cd' to go back to the home directory is not a good approach here as the starting directory of the bash is not the home directory of the machine. so basically had to use "../" to move to the parent directory after going into dir2.

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -36,8 +36,9 @@ rm file4.txt
 
 # 7. Delete directories dir4 and dir5 including all their contents (if any)
 
-cd
-rm -r dir4 dir5
+cd ../
+rm -r dir4
+rm -r dir5
 
 # 8. List the contents of the current working directory to verify the deletion of dir4 and dir5
 
